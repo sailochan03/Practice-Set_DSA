de72de1659556a8e7db56341af501487532f3e40
Sai Lochan Dash <128344365+smix23@users.noreply.github.com>
2023-05-07 08:57:03 +0530

    Update ps1q5.java

--- ps1q5.java
+++ ps1q5.java
@@ -5,23 +5,32 @@ money to another account */
 
 import java.util.Scanner;
 
-
 class BankAccount
 {
-    String name;
-    static double bal=0; 
+    String name1, name2;
+    static double bal1=0;
+    static double bal2=0; 
     double depo, with;
     
-    void detail_input(String n, double b)
+    void detail_input(String n1, String n2, double b1, double b2)
     { 
-        name = n;
-        bal = b;
+        name1 = n1;
+        name2 = n2;
+        bal1 = b1;
+        bal2 = b2;
     }
     void detail_display()
     {
         System.out.println("");
-        System.out.println("ACCOUNT HOLDER: "+name);
-        System.out.println("CURRENT BALANCE: Rs."+bal);
+        System.out.println("ACCOUNT HOLDER: "+name1);
+        System.out.println("CURRENT BALANCE: Rs."+ bal1);
+        System.out.println("");
+    }
+    void detail_display2()
+    {
+        System.out.println("");
+        System.out.println("ACCOUNT HOLDER: "+name2);
+        System.out.println("CURRENT BALANCE: Rs."+ bal2);
         System.out.println("");
     }
 
@@ -31,9 +40,9 @@ class BankAccount
         System.out.println("Enter deposit amount: ");
         depo = scan.nextDouble();
         
-        bal = bal + depo;
+         bal1 =  bal1 + depo;
         
-        System.out.println("New Balance: "+bal);    
+        System.out.println("New Balance: "+ bal1);    
     }
     void withdraw()
     {
@@ -41,9 +50,21 @@ class BankAccount
         System.out.println("Enter deposit amount: ");
         with = scan.nextDouble();
 
-        bal = bal - with;
+         bal1 =  bal1 - with;
 
-        System.out.println("New Balance: "+bal);    
+        System.out.println("New Balance: "+ bal1);    
+    }
+    void transfer()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter amount to Transfer: ");
+        double t = scan.nextDouble();
+
+        bal1 = bal1 - t;
+        bal2 = bal2 + t;
+
+        System.out.println("AMOUNT TRANSFERRED SUCCESFULLY.");
+    
     }
 
 }
@@ -55,16 +76,26 @@ public class ps1q5
         Scanner scan = new Scanner(System.in);
         BankAccount b = new BankAccount();
         
-        b.detail_input("Sai Lochan Dash", 10000.0);
-        b.detail_display();
+        b.detail_input("Sai Lochan Dash", "MUKESH AMBANI", 10000.0, 10000.0);
 
-        System.out.println("1 for deposit, 2 for withdrawl: ");
+        System.out.println("1 for deposit, 2 for withdrawl, 3 for transfer: ");
         int i = scan.nextInt();
 
         if(i==1)
-        b.deposit();  
-        else if(i ==2)
-        b.withdraw();
+        {
+            b.deposit();
+            b.detail_display();
+        }
+        else if(i==2)
+        {
+            b.withdraw();
+            b.detail_display();
+        }
+        else if(i==3)
+        {
+            b.transfer();
+            b.detail_display2();
+        }
         else
         System.out.println("INVALID INPUT.");
     }
